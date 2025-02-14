78796d6a6544751953987500607871c64e23b329
Day <tristan.marty240112@gmail.com>
2025-02-14 15:21:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-const { Client, logger } = require('./lib/client')
-const { DATABASE, VERSION } = require('./config')
-const { stopInstance } = require('./lib/pm2')
+const { spawnSync } = require('child_process')
+const { existsSync, writeFileSync } = require('fs')
 
-const start = async () => {
-  logger.info(`levanter ${VERSION}`)
-  try {
-    await DATABASE.authenticate({ retry: { max: 3 } })
-  } catch (error) {
-    const databaseUrl = process.env.DATABASE_URL
-    logger.error({ msg: 'Unable to connect to the database', error: error.message, databaseUrl })
-    return stopInstance()
+const SESSION_ID = 'levanter_148eae7c7669b148fabcb31be926df702f' // Edit this line only, don't remove ' <- this symbol
+
+if (!existsSync('levanter')) {
+  console.log('Cloning the repository...')
+  const cloneResult = spawnSync(
+    'git',
+    ['clone', 'https://github.com/lyfe00011/levanter.git', 'levanter'],
+    {
+      stdio: 'inherit',
+    }
+  )
+
+  if (cloneResult.error) {
+    throw new Error(`Failed to clone the repository: ${cloneResult.error.message}`)
   }
+
+  const configPath = 'levanter/config.env'
   try {
-    const bot = new Client()
-    await bot.connect()
-  } catch (error) {
-    logger.error(error)
+    console.log('Writing to config.env...')
+    writeFileSync(configPath, `VPS=true\nSESSION_ID=${SESSION_ID}`)
+  } catch (err) {
+    throw new Error(`Failed to write to config.env: ${err.message}`)
+  }
+
+  console.log('Installing dependencies...')
+  const installResult = spawnSync('yarn', ['install', '--network-concurrency', '3'], {
+    cwd: 'levanter',
+    stdio: 'inherit',
+  })
+
+  if (installResult.error) {
+    throw new Error(`Failed to install dependencies: ${installResult.error.message}`)
   }
 }
-start()
+
+spawnSync('yarn', ['start'], { cwd: 'levanter', stdio: 'inherit' })
